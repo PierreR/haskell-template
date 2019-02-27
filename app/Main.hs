c58b31264951d08c114916cc8f07e8fba540a989
Pierre Radermecker <pradermecker@cirb.brussels>
2019-02-27 20:27:12 +0100

    Add some UIx to RPN

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,41 @@
+{-# LANGUAGE OverloadedLists #-}
 module Main where
 
-import qualified Data.Text        as Text
-import qualified Data.Text.IO     as Text
+-- import qualified Data.Text        as Text
+-- import qualified Data.Text.IO     as Text
 -- import qualified Data.Text.Lazy   as Text.Lazy
+import qualified Data.Text.Read as Text
+import Template.Math.RPN
+import qualified Data.HashMap.Strict as Map
 
 import Template.Prelude
 
+printResult :: MonadIO io => Text -> EnvVars -> io ()
+printResult expr env = either putStrLn print (evalRPN expr env)
+
+getEnvVar :: IO EnvVars
+getEnvVar = do
+  putText "Need to set values ? (n or enter)"
+  i <- getLine
+  if (i == "n")
+    then
+      pure mempty
+    else do
+      putText "Variable name ?"
+      var <- getLine
+      putText "Value ?"
+      val0 <- getLine
+      let read_val = hush . second fst . Text.decimal
+      case read_val val0 of
+        Nothing -> do
+          putText $  "Sorry cannot convert the value " <> val0 <> " into an integer"
+          putText $ "Let's try again"
+          getEnvVar
+        Just v -> pure $ Map.singleton var v
+
 main :: IO ()
-main = putText "hello"
+main = do
+  putText "Enter an expression in RPN (Reverse Polish Notation) and I will try to resolve it"
+  expr <- getLine
+  val <- getEnvVar
+  printResult expr val
--- src/Template/Math/RPN.hs
+++ src/Template/Math/RPN.hs
@@ -1,16 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Template.Math.RPN
-  ( evalRPN)
+  (evalRPN, EnvVars)
 where
 
-import Template.Prelude
+import Template.Prelude hiding (show)
 import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 import qualified Data.Text.Read as Text
+import qualified Data.HashMap.Strict as Map
 
+-- import GHC.Show (Show(..))
+
+type EnvVars = Map.HashMap Text Integer
 type Stack = [Integer]
-type ErrMsg = Text
-type EvalM = ExceptT ErrMsg (State Stack)
+type EvalM = ReaderT EnvVars (ExceptT EvalError (State Stack))
+
+data EvalError = NotEnoughElements | NotANumber Text | ExtraElements
+
+printNotANumber err = "Can't convert " <> err <> " into an integer"
+printExtraElements = "The expression could not be fully resolved"
+printNotEnoughElements = "Not enough elements in the expression"
+
+instance Print EvalError where
+  hPutStr h (NotANumber err) = liftIO $ Text.hPutStr h (printNotANumber err)
+  hPutStr h ExtraElements = liftIO $ Text.hPutStr h printExtraElements
+  hPutStr h NotEnoughElements = liftIO $ Text.hPutStr h printNotEnoughElements
+  hPutStrLn h (NotANumber err) = liftIO $ Text.hPutStrLn h (printNotANumber err)
+  hPutStrLn h ExtraElements = liftIO $ Text.hPutStrLn h printExtraElements
+  hPutStrLn h NotEnoughElements = liftIO $ Text.hPutStrLn h printNotEnoughElements
 
 push :: Integer -> EvalM ()
 push x = modify (x:)
@@ -19,28 +37,32 @@ pop :: EvalM Integer
 pop = do
   xs <- get
   when (null xs) $
-    throwError "Trying to pop from an empty stack"
+    throwError NotEnoughElements
   put (tailSafe xs)
   case headMay xs of
     Just s -> pure s
-    Nothing -> throwError "The head of the stack is empty"
+    Nothing -> throwError NotEnoughElements -- "The head of the stack is empty"
 
 checkResult :: EvalM ()
 checkResult = do
   xs <- get
   when (length xs /= 1) $
-    throwError "The expression could not be fully resolved"
+    throwError ExtraElements
+
+handleNaN :: Text -> Maybe Integer -> EvalM Integer
+handleNaN s Nothing = throwError (NotANumber s)
+handleNaN _ (Just n ) = pure n
 
 readSafe :: Text -> EvalM Integer
 readSafe t = do
-  let err = "Can't convert " <> t <> " into an integer"
-  ExceptT $ pure $ bimap (const err) fst (Text.decimal t)
-  -- case Text.decimal t of
-  --   Right (s,_) -> pure s
-  --   Left _ -> throwError err
+  readSafe' t `catchError` handler
+  where
+    readSafe' t = handleNaN t $ hush $ second fst (Text.decimal t)
+    handler (NotANumber n) = asks (Map.lookup n) >>= handleNaN t
+    handler e = throwError e
 
-evalRPN :: Text -> Either ErrMsg Integer
-evalRPN expr = evalState (runExceptT evalRPN') []
+evalRPN :: Text -> EnvVars -> Either EvalError Integer
+evalRPN expr env = evalState (runExceptT (runReaderT evalRPN' env)) []
   where
     evalRPN' = traverse_ step (Text.words expr) >> checkResult >> pop
     step "+" = process_tops (+)
